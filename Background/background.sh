60b5fe5b19a735e655754a0e726778898a3e8bc0
R. Diez <rdiezmail-tools@yahoo.de>
2014-10-22 20:42:07 +0200

    Improvements to background.sh: - Option to use ionice. - Optional -- argument to delimit end of command-line switches.

--- Background/background.sh
+++ Background/background.sh
@@ -4,8 +4,32 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Here you can set the method this tool uses to run processes with a lower priority:
+# - Method "nice" uses "nice -n 15". This is normally the best choice, as it has an impact
+#   on both CPU and disk priority.
+# - Method "ionice" uses "ionice --class 2 --classdata 7".
+#   Class 2 means "best-effort" and is equivalent to the default ionice priority.
+#   Priority 7 is the lowest priority in the "best-effort" class.
+#   This affects only disk I/O priority. You may want to switch to this method
+#   if you are running long background calculations (like BOINC with SETI@home)
+#   and you are using the "ondemand" CPU scaling governor with setting "ignore_nice_load"
+#   enabled in order to keep your laptop from heating up and its fan from getting loud.
+#   Otherwise, any process started with background.sh will run more slowly than
+#   it probably should.
+# - Method "none" does not set the child process' priority.
 
-VERSION_NUMBER="2.0"
+LOW_PRIORITY_METHOD="nice"
+
+# If you are using "ignore_nice_load", and have adjusted your 'nice' level before,
+# you may find it handy to check the current 'nice' level every time this script runs.
+# This way, you will realise straight away if you have one 'nice' too much somewhere
+# you do not remember. This script is actually designed to be used interactively
+# in a normal scenario. If you (or some automated script) have adjusted the 'nice' level
+# beforehand, the resulting processing priority will probably not be what you expect.
+
+CHECK_NICE_LEVEL=false
+
+VERSION_NUMBER="2.1"
 LOG_FILENAME="BackgroundCommand.log"
 ABS_LOG_FILENAME="$(readlink -f "$LOG_FILENAME")"
 
@@ -23,13 +47,13 @@ display_help ()
   echo "background.sh version $VERSION_NUMBER"
   echo "Copyright (c) 2011-2014 R. Diez - Licensed under the GNU AGPLv3"
   echo
-  echo "This tool runs the given process under a combination of ('nice -n 15' + 'time' + 'tee') commands and displays a visual notification when finished."
+  echo "This tool runs the given process with a low priority under a combination of ('time' + 'tee') commands and displays a visual notification when finished."
   echo
   echo "The visual notification consists of a transient desktop taskbar indication (if command 'notify-send' is installed) and a permanent modal message box. If you are sitting in front of the screen, the taskbar notification should catch your attention, even if the dialog box remains hidden beneath other windows. Should you miss the notification, the dialog box remains there until manually closed."
   echo
   echo "This tool is useful in the following scenario:"
   echo "- You need to run a long process, such as copying a large number of files or recompiling a big software project."
-  echo "- You want to carry on using the computer for other tasks. That long process should run with a low CPU and disk priority in the background (command 'nice -n 15')."
+  echo "- You want to carry on using the computer for other tasks. That long process should run with a low CPU and/or disk priority in the background. By default, the process' priority is reduced with 'nice -n 15', but you can switch to 'ionice', see variable LOW_PRIORITY_METHOD in this script's source code for more information."
   echo "- You want to leave the process' console (or emacs frame) open, in case you want to check its progress in the meantime."
   echo "- You might inadvertently close the console window at the end, so you need a log file with all the console output for future reference (the 'tee' command)."
   echo "- You may not notice when the process has completed, so you would like a visible notification in your windowing environment (like KDE)."
@@ -39,7 +63,7 @@ display_help ()
   echo "- All that should work under Cygwin on Windows too."
   echo
   echo "Syntax:"
-  echo "  background.sh <options...> command <command arguments...>"
+  echo "  background.sh <options...> <--> command <command arguments...>"
   echo  
   echo "Options:"
   echo " --help     displays this help text"
@@ -47,8 +71,8 @@ display_help ()
   echo " --license  prints license information"
   echo
   echo "Usage examples:"
-  echo "  ./background.sh echo \"Long process runs here...\""
-  echo "  ./background.sh sh -c \"exit 5\""
+  echo "  ./background.sh -- echo \"Long process runs here...\""
+  echo "  ./background.sh -- sh -c \"exit 5\""
   echo
   echo "Caveat: If you start several instances of this script, you should do it from different directories, as the log filename is hard-coded to \"$LOG_FILENAME\" and it will be overwritten each time."
   echo
@@ -104,6 +128,7 @@ case "$1" in
   --version)
     echo "$VERSION_NUMBER"
     exit 0;;
+  --) shift;;
   --*) abort "Unknown option \"$1\".";;
 esac
 
@@ -122,6 +147,15 @@ set -o errexit
 
 if [ $EXTERNAL_TIME_COMMAND_EXIT_CODE -ne 0 ]; then
   abort "The external 'time' command was not found. You may have to install it with your Operating System's package manager. For example, under Cygwin the associated package is called \"time\", and its description is \"The GNU time command\"."
+fi
+
+if $CHECK_NICE_LEVEL; then
+  declare -i CURRENT_NICE_LEVEL="$(nice)"
+  declare -i EXPECTED_NICE_LEVEL=0
+
+  if (( CURRENT_NICE_LEVEL != EXPECTED_NICE_LEVEL )); then
+    abort "The current 'nice' level of $CURRENT_NICE_LEVEL does not match the expected value of $EXPECTED_NICE_LEVEL."
+  fi
 fi
 
 
@@ -206,7 +240,12 @@ printf "\n"
 set +o errexit
 set +o pipefail
 
-"$EXTERNAL_TIME_COMMAND" $TIME_ARG_QUIET -f "\nElapsed time running command: %E"  nice -n 15  "$@" 2>&1 | tee "$LOG_FILENAME"
+case "$LOW_PRIORITY_METHOD" in
+  none) "$EXTERNAL_TIME_COMMAND" $TIME_ARG_QUIET -f "\nElapsed time running command: %E"  "$@" 2>&1 | tee "$LOG_FILENAME";;
+  nice) "$EXTERNAL_TIME_COMMAND" $TIME_ARG_QUIET -f "\nElapsed time running command: %E"  nice -n 15 -- "$@" 2>&1 | tee "$LOG_FILENAME";;
+  ionice) "$EXTERNAL_TIME_COMMAND" $TIME_ARG_QUIET -f "\nElapsed time running command: %E"  ionice --class 2 --classdata 7 --  "$@" 2>&1 | tee "$LOG_FILENAME";;
+  *) abort "Unknown LOW_PRIORITY_METHOD \"$LOW_PRIORITY_METHOD\".";;
+esac
 
 # Copy the exit status array, or it will get lost when the next command executes.
 declare -a CAPTURED_PIPESTATUS=( ${PIPESTATUS[*]} )
