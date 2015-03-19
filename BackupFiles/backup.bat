3289ad0630bfb0c2404188fddaeb9395eb2fae63
R. Diez <rdiezmail-tools@yahoo.de>
2015-03-19 18:28:19 +0100

    backup.bat: small improvements

--- BackupFiles/backup.bat
+++ BackupFiles/backup.bat
@@ -3,13 +3,39 @@
 rem  This is the script template I normally use to back up my files under Windows.
 rem  See the Linux sibling script 'backup.sh' for more information.
 rem
+rem  Before running this script, copy it to an empty directory and edit the directory paths
+rem  to backup and the subdirectories and file extensions to exclude. The resulting
+rem  backup files will be placed in the current (initially empty) directory.
+rem
+rem  If you are backing up to an external disk, beware that the compressed files will be
+rem  read back in order to create the redundancy data. If the external disk is slow,
+rem  it may take a long time. Therefore, you may want to create the backup files on your
+rem  primary disk first and move the resulting files to the external disk afterwards.
+rem
+rem  About the par2 tool that creates the redundancy information:
+rem  - You can either use the classic 'par2' at:
+rem      sourceforge.net/projects/parchive
+rem    (look for 'par2cmdline')
+rem  - Or you can download a faster, parallel version from:
+rem      http://chuchusoft.com/par2_tbb/
+rem
 rem  Copyright (c) 2015 R. Diez
 rem  Licensed under the GNU Affero General Public License version 3.
 
-set TARBALL_FILENAME=MyBackupFiles.7z
+
+set BASE_FILENAME=MyBackupFiles
+
+set REDUNDANCY_PERCENTAGE=1
 
 set TOOL_7Z="%ProgramFiles%\7-Zip\7z.exe"
 set TOOL_PAR2="par2.exe"
+
+
+rem  The PAR2 files do not have ".7z" in their names, in order to
+rem  prevent any possible confusion. Otherwise, a wildcard glob like "*.7z.*" when
+rem  building the PAR2 files might include any existing PAR2 files again,
+rem  which is a kind of recursion to avoid.
+set TARBALL_FILENAME=%BASE_FILENAME%.7z
 
 
 if exist %TOOL_7Z% goto carryOn2a
@@ -24,17 +50,17 @@ if exist %TOOL_PAR2% goto carryOn2b
 WHERE %TOOL_PAR2% 2>NUL
 IF %ERRORLEVEL% EQU 0 goto carryOn2b
 echo Error: Tool %TOOL_PAR2% not found.
-echo You can download it from sourceforge.net/projects/parchive, look for 'par2cmdline'.
+echo See the comments in this script for some alternatives.
 exit /b 1
 :carryOn2b
 
 
 rem  Delete any previous backup files, which is convenient if you modify and re-run this script.
-if exist "%TARBALL_FILENAME%.*" goto filesExist
+if exist "%BASE_FILENAME%.*" goto filesExist
 goto carryOn3
 :filesExist
 echo Deleting existing backup files...
-del "%TARBALL_FILENAME%.*"
+del "%BASE_FILENAME%.*"
 :carryOn3
 
 
@@ -58,11 +84,12 @@ rem
 rem   - Exclude all *.bak files (by extension):
 rem     -xr!*.bak
 rem
-rem  When testing this script, you may want to replace switch -mx1 with -mx0 (no compression, much faster).
+rem  When testing this script, you may want to temporarily replace switches "-m0=Deflate -mx1" with
+rem  "-m0=Copy", in order to skip the slow compression algorithm.
 rem  You may also want to temporarily remove the -p (password) switch.
 
 start "" /BELOWNORMAL /B /WAIT ^
-  %TOOL_7Z% a -t7z "%TARBALL_FILENAME%" -m0=Deflate -mx1 -mmt -ms -mhe=on -v2g -p ^
+  %TOOL_7Z% a -t7z "%TARBALL_FILENAME%" -m0=Deflate -mx1 -mmt -ms -mhe=on -v2g -p -- ^
   ^
   "-x!dirToBackup1\skipThisParticularDir\Subdir1" ^
   "-x!dirToBackup1\skipThisParticularDir\Subdir2" ^
@@ -84,15 +111,14 @@ set EXIT_CODE=%ERRORLEVEL%
 IF %EXIT_CODE% NEQ 0 GOTO CommandFailed
 
 echo Building redundant records...
-set REDUNDANCY_PERCENTAGE=1
 start "" /BELOWNORMAL /B /WAIT ^
-  %TOOL_PAR2%  create -q -r%REDUNDANCY_PERCENTAGE% "%TARBALL_FILENAME%.par2" "%TARBALL_FILENAME%.*"
+  %TOOL_PAR2%  create -q -r%REDUNDANCY_PERCENTAGE% -- "%BASE_FILENAME%.par2" "%TARBALL_FILENAME%.*"
 set EXIT_CODE=%ERRORLEVEL%
 
 
 rem Notify that we have finished.
 
-set VBS_FILENAME=%TARBALL_FILENAME%.notification-dialog.vbs
+set VBS_FILENAME="%BASE_FILENAME%.notification-dialog.vbs"
 echo Option Explicit>%VBS_FILENAME%
 
 IF %EXIT_CODE% EQU 0 GOTO carryOn4
@@ -107,6 +133,7 @@ echo MsgBox "The command finished successfully.", vbOKOnly, "Background cmd OK">
 echo WScript.Quit(0)>>%VBS_FILENAME%
 echo Waiting for the user to close the notification dialog window...
 cscript //nologo %VBS_FILENAME%
+del %VBS_FILENAME%
 IF %EXIT_CODE% EQU 0 echo Finished creating backup.
 IF %EXIT_CODE% NEQ 0 echo Finished with error.
 exit /b %EXIT_CODE%
