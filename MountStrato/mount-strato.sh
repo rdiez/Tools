8881de455444f43f1328133d78a5f68f24de43a8
R. Diez <rdiezmail-tools@yahoo.de>
2016-05-08 17:56:34 +0200

    mount-strato.sh: Added method 'gvfs-mount-webdav'.

--- MountStrato/mount-strato.sh
+++ MountStrato/mount-strato.sh
@@ -3,19 +3,32 @@
 # Copyright (c) 2015-2016 R. Diez - Licensed under the GNU AGPLv3
 # Please send feedback to rdiezmail-tools@yahoo.de
 #
+# USAGE:
+#
 # This is the script I use to 'comfortably' mount the Strato Hidrive over WebDAV on my Linux PC,
 # and then mount an EncFS encrypted filesystem (or 2 of them) on top of it.
 #
+# Without any arguments, the script will mount the filesystems mentioned above.
 # If you specify "umount" or "unmount" as the first (and only) command-line argument,
 # it will unmount all filesystems.
 #
-# This script needs the WebDAV Linux File System (package 'davfs2' on Ubuntu/Debian).
-# If you haven't disabled the local password encryption, it also needs tool 'scrypt' (package 'scrypt').
-# I had trouble with davfs2, it often managed to hang my whole KDE session. In order
-# to alleviate the problem, I edited file /etc/davfs2/davfs2.conf as root and
-# added the following settings:
-#   use_locks 0
-#   gui_optimize 1
+# PREREQUISITES:
+#
+# - For a MOUNT_METHOD_UNENCRYPTED of davfs2, this script needs the WebDAV Linux File System
+#   (package 'davfs2' on Ubuntu/Debian).
+#
+# - For a MOUNT_METHOD_UNENCRYPTED of gvfs-mount-webdav:
+#
+#   - You have to install GVFS and FUSE support on your Linux OS beforehand. On Debian, the packages
+#     are called "gvfs-bin", "gvfs-backends" and "gvfs-fuse". You can install them with the
+#     following command:
+#       sudo apt-get install gvfs-bin gvfs-backends gvfs-fuse
+#
+#   - Your user account must be a member of the "fuse" group. You can do that with the
+#     following command:
+#       sudo adduser "$USER" fuse
+#
+# INSTALLATION:
 #
 # In order to use this script, you will have to edit STRATO_USERNAME and so on below in order
 # to match your system configuration.
@@ -40,31 +53,67 @@
 # - Run this command (the filename must mach variable LONG_PASSWORD_FILENAME below):
 #     scrypt enc /tmp/My-$UID.tmp >"$HOME/StratoHidrive/StratoHidriveEncfsLongPassword1.txt"
 # - Don't forget to delete file /tmp/My-$UID.tmp afterwards.
-# - You will have to manually mount your Strato Hidrive once (without this script),
+# - You will have to mount your Strato Hidrive once (turn off MOUNT_ENCRYPTED_1 and MOUNT_ENCRYPTED_2),
 #   create a "Data1" directory (see ENCRYPTED_FS_PATH_1 below), and run the following
 #   command (with your Strato username instead of $STRATO_USERNAME):
 #     encfs "$HOME/StratoHidrive/MountpointUnencrypted/users/$STRATO_USERNAME/Data1" "$HOME/StratoHidrive/EncFs"
 # - Unmount it with:
 #     fusermount -u "$HOME/StratoHidrive/EncFs"
 #
-# Afterwards, this script should be able to mount the Hidrive and the EncFS filesystem on top
-# with a minimum of fuss. You still need to type your root (sudo) password though, because
-# it mounts the Hidrive with 'sudo mount'. I hope one day I will finally manage
-# to get gvfs-mount to work, so that you do not need the root password anymore.
+# Afterwards, this script should be able to mount the Hidrive and the EncFS filesystems on top
+# with a minimum of fuss.
 #
-# If you do not want to be prompted for your Strato username and password every time,
-# edit file "/etc/davfs2/secrets" as root and add a line like the following.
-# Note that your password will then be visible in plain text to anybody with root privileges.
+# FURTHER NOTES:
 #
-#   https://your_strato_username.webdav.hidrive.strato.com/ your_strato_username your_strato_password
+# Further notes for a MOUNT_METHOD_UNENCRYPTED of davfs2:
 #
-# Alternatively, if you add option username="your_strato_username" to the mount -o flags below,
-# you will only be prompted for your Strato password (and not for your Strato username).
-# Note that your Strato username will then be visible to everyone in the output of the 'ps' command.
+#   - You need to type your root (sudo) password every time (if sudo has not cached it),
+#     because this script mounts the Hidrive with 'sudo mount'.
 #
-# Strato HiDrive has not been very reliable in my experience. Sometimes, files land in the 'lost+found'
-# directory for no apparent reason. Therefore, this script checks whethere there are any files there
-# and opens a file explorer window on it, so that they do not remain unnoticed for a long time.
+#   - If you do not want to be prompted for your Strato username and password every time,
+#     edit file "/etc/davfs2/secrets" as root and add a line like the following.
+#     Note that your password will then be visible in plain text to anybody with root privileges.
+#
+#       https://your_strato_username.webdav.hidrive.strato.com/ your_strato_username your_strato_password
+#
+#     Alternatively, if you add option username="your_strato_username" to the mount -o flags below,
+#     you will only be prompted for your Strato password (and not for your Strato username).
+#     Note that your Strato username will then be visible to everyone in the output of the 'ps' command.
+#
+#   - I had trouble with davfs2, it often managed to hang my whole KDE session.
+#     "davfs2 often freezes any program that uses it"
+#     I reported the bug here:
+#       https://bugs.launchpad.net/ubuntu/+source/davfs2/+bug/1538445
+#     In order to alleviate the problem, I edited file /etc/davfs2/davfs2.conf as root and
+#     added the following settings:
+#       use_locks 0
+#       gui_optimize 1
+#     This work-around does not suffice, sometimes davfs2 still hangs.
+#
+#   - Strato HiDrive has not been very reliable in my experience. Sometimes, files land in the 'lost+found'
+#     directory for no apparent reason. I am not sure yet whether this happens because of the Strato
+#     servers, or because of an davfs2 issue. Therefore, this script checks whethere there are any files there
+#     and opens a file explorer window on it, so that they do not remain unnoticed for a long time.
+#
+# Further notes for a MOUNT_METHOD_UNENCRYPTED of gvfs-mount-webdav:
+#
+#   - If you do not want to be prompted for your Strato password every time, enter your password
+#     in variable STRATO_PASSWORD below. Note that your password will then be visible in plain text
+#     to anybody that can read this script file.
+#
+#   - If you are having trouble unmounting a GVFS mount point because it is still in use,
+#     command "lsof | grep ^gvfs" might help. Tool "gvfs-mount --unmount" does not seem
+#     to have a "lazy unmount" option like 'umount' has.
+#
+#   - If you type the wrong password, tool 'gvfs-mount' will enter an infinite loop (as of Kubuntu 14.04 in Oct 2014,
+#     gvfs version 1.20.1). As a result, this script will appear to hang.
+#     The reason is that gvfs-mount does not realise when the stdin file descriptor reaches the end of file,
+#     which is the case as this scripts redirects stdin in order to feed it with the password.
+#     The only way out is to press Ctrl+C to interrupt the script together with all its child processes.
+#     I reported this issue (see bug 742942 in https://bugzilla.gnome.org/) and it has been fixed for version 1.23).
+#
+#   - This method does not really work for me, as EncFS fails to update files over a WebDAV gvfs-mount.
+#     I am still looking for the cause to this serious issue.
 
 set -o errexit
 set -o nounset
@@ -81,11 +130,22 @@ MOUNT_ENCRYPTED_1=true
 MOUNT_ENCRYPTED_2=false
 
 # Unencrypted data.
-MOUNT_METHOD_UNENCRYPTED="webdav"
+# Mount methods are: davfs2, gvfs-mount-webdav
 STRATO_USERNAME="your_strato_username_here"
+
+# This password is only used for method gvfs-mount-webdav.
+STRATO_PASSWORD="your_password_here"
+
+MOUNT_METHOD_UNENCRYPTED="davfs2"
 BASE_DIR="$HOME/StratoHidrive"
-MOUNT_POINT_UNENCRYPTED="$BASE_DIR/MountpointUnencrypted"
+# Whereas the 'davfs2' method needs an existing directory as mountpoint, the 'gvfs-mount-webdav'
+# method generates the mountpoint at a system-defined location.
+# This script creates the following link, which is independent of the mount method.
+MOUNT_LINK="$BASE_DIR/RemoteUnencrypted"
+MOUNT_POINT_UNENCRYPTED="$BASE_DIR/MountpointUnencrypted"  # Only used for the 'davfs2' method.
 OPEN_EXPLORER_WINDOW_ON_MOUNT_UNENCRYPTED=false
+
+USER_SUBDIR="users/$STRATO_USERNAME"
 
 
 # Encrypted filesystem 1.
@@ -97,7 +157,7 @@ OPEN_EXPLORER_WINDOW_ON_MOUNT_ENCRYPTED_1=true
 IS_LONG_PASSWORD_ENCRYPTED_1=false
 
 LONG_PASSWORD_FILENAME_1="$BASE_DIR/StratoHidriveEncfsLongPassword1.txt"
-ENCRYPTED_FS_PATH_1="$MOUNT_POINT_UNENCRYPTED/users/$STRATO_USERNAME/Data1"
+ENCRYPTED_FS_PATH_1="$MOUNT_LINK/$USER_SUBDIR/Data1"
 
 
 # Encrypted filesystem 2.
@@ -109,7 +169,7 @@ OPEN_EXPLORER_WINDOW_ON_MOUNT_ENCRYPTED_2=false
 IS_LONG_PASSWORD_ENCRYPTED_2=false
 
 LONG_PASSWORD_FILENAME_2="$BASE_DIR/StratoHidriveEncfsLongPassword2.txt"
-ENCRYPTED_FS_PATH_2="$MOUNT_POINT_UNENCRYPTED/users/$STRATO_USERNAME/Data2"
+ENCRYPTED_FS_PATH_2="$MOUNT_LINK/$USER_SUBDIR/Data2"
 
 
 # ---- You probably do not need to change anything below this point ----
@@ -151,6 +211,95 @@ is_dir_empty ()
       echo "Files found: ${FILES[@]}"
     fi
     return $BOOLEAN_FALSE
+  fi
+}
+
+
+# This routine exists because sometimes I have seen "readlink -f" failing to deliver the symlink target
+# without printing any error message at all. Sometimes, trying to access a just-mounted Windows share
+# yields error "cannot access /run/user/1000/gvfs/smb-share:domain=blah,server=blah,share=blah,user=blah: Input/output error",
+# and seemingly this causes "readlink -f" to silently fail. At least it returns a non-zero status code.
+#
+# It looks like readlink's flag "-f" makes it actually try to access the remote server. I have removed that flag,
+# because we do not actually need the absolute, canonical path here. This way, readlink always succeed,
+# even if the remote server is not accessible yet.
+
+get_link_target ()
+{
+  set +o errexit
+
+  EXISTING_LINK_TARGET="$(readlink -- "$1")"
+
+  local EXIT_CODE="$?"
+
+  set -o errexit
+
+  if (( EXIT_CODE != 0 )); then
+    abort "Cannot read the target for symbolic link \"$1\", readlink failed with exit code $EXIT_CODE."
+  fi
+
+  if [[ $EXISTING_LINK_TARGET = "" ]]; then
+    abort "Cannot read the target for symbolic link \"$1\", readlink returned an empty string for that symlink."
+  fi
+}
+
+
+create_or_update_symbolic_link ()
+{
+  local LINK_FILENAME="$1"
+  local TARGET_FILENAME="$2"
+
+  local PRINT_LINK_INFO=false
+
+  if [ -h "$LINK_FILENAME" ]; then
+    # The file exists and is a symbolic link.
+
+    local EXISTING_LINK_TARGET
+    get_link_target "$LINK_FILENAME"
+
+    if [[ $EXISTING_LINK_TARGET == "$TARGET_FILENAME" ]]; then
+      if $PRINT_LINK_INFO; then
+        printf "\"%s\" -> \"%s\" (symlink already existed)\n" "$LINK_FILENAME" "$TARGET_FILENAME"
+      fi
+    else
+      if $PRINT_LINK_INFO; then
+        printf "\"%s\" -> \"%s\" (rewriting symlink)\n" "$LINK_FILENAME" "$TARGET_FILENAME"
+      fi
+      rm -- "$LINK_FILENAME"
+      ln --symbolic "$TARGET_FILENAME" "$LINK_FILENAME"
+    fi
+
+  elif [ -e "$LINK_FILENAME" ]; then
+
+    abort "Error creating symbolic link: File \"$LINK_FILENAME\" exists but is not a symbolic link. I am not sure whether I should delete it."
+
+  else
+
+    if $PRINT_LINK_INFO; then
+      printf "\"%s\" -> \"%s\" (creating symlink)\n" "$LINK_FILENAME" "$TARGET_FILENAME"
+    fi
+    ln --symbolic "$TARGET_FILENAME" "$LINK_FILENAME"
+
+  fi
+}
+
+
+delete_symbolic_link ()
+{
+  local LINK_FILENAME="$1"
+
+  if [ -h "$LINK_FILENAME" ]; then
+    # The file exists and is a symbolic link.
+    rm -- "$LINK_FILENAME"
+
+  elif [ -e "$LINK_FILENAME" ]; then
+
+    abort "Error deleting symbolic link: File \"$LINK_FILENAME\" exists but is not a symbolic link. I am not sure whether I should delete it."
+
+  else
+
+    echo "Nothing do to here." >/dev/null
+
   fi
 }
 
@@ -214,11 +363,15 @@ if $SHOULD_MOUNT; then
 
   if $MOUNT_UNENCRYPTED; then
 
-    mkdir --parents "$MOUNT_POINT_UNENCRYPTED"
-
-    if ! is_dir_empty "$MOUNT_POINT_UNENCRYPTED"; then
-      abort "Mount point \"$MOUNT_POINT_UNENCRYPTED\" is not empty (already mounted?). While not strictly a requirement for mounting purposes, this script does not expect a non-empty mountpoint."
-    fi
+    case "$MOUNT_METHOD_UNENCRYPTED" in
+      davfs2)  mkdir --parents "$MOUNT_POINT_UNENCRYPTED"
+               if ! is_dir_empty "$MOUNT_POINT_UNENCRYPTED"; then
+                 abort "Mount point \"$MOUNT_POINT_UNENCRYPTED\" is not empty (already mounted?). While not strictly a requirement for mounting purposes, this script does not expect a non-empty mountpoint."
+               fi
+               ;;
+      gvfs-mount-webdav) echo "Nothing to do here." >/dev/null;;
+      *) abort "Unsupported mount method \"$MOUNT_METHOD_UNENCRYPTED\" for unencrypted filesystem.";;
+    esac
   fi
 
   if $MOUNT_ENCRYPTED_1; then
@@ -260,18 +413,52 @@ if $SHOULD_MOUNT; then
 
   if $MOUNT_UNENCRYPTED; then
 
+    echo "If you wish to connect with a GIO (a GNOME VFS API) client like Nautilus, the URL is:"
+    echo "  davs://$STRATO_USERNAME@$STRATO_USERNAME.webdav.hidrive.strato.com:443/$USER_SUBDIR"
+    echo "This uses the same system as gvfs-mount, so you will not need to enter your WebDAV password again if you are using the 'gvfs-mount' method."
+    echo "If you wish to connect with a KIO (KDE Input/Output) client like Konqueror and Dolphin, the URL is:"
+    echo "  webdavs://$STRATO_USERNAME@$STRATO_USERNAME.webdav.hidrive.strato.com:443/$USER_SUBDIR"
+    echo "Apparently, this is a separate system, so you will need to type your WebDAV password again."
+    echo "Firefox supports WebDAV natively, the URL is:"
+    echo "  https://$STRATO_USERNAME@$STRATO_USERNAME.webdav.hidrive.strato.com:443/$USER_SUBDIR"
+
     echo "Mounting the Strato Hidrive..."
 
     case "$MOUNT_METHOD_UNENCRYPTED" in
-      webdav)  CMD="sudo mount -t davfs -o uid=\"$UID\",gid=\"$UID\" -- \"https://$STRATO_USERNAME.webdav.hidrive.strato.com/\" \"$MOUNT_POINT_UNENCRYPTED\"";;
+      davfs2)  CMD="sudo mount -t davfs -o uid=\"$UID\",gid=\"$UID\" -- \"https://$STRATO_USERNAME.webdav.hidrive.strato.com/\" \"$MOUNT_POINT_UNENCRYPTED\""
+               echo "$CMD"
+               eval "$CMD"
+               ;;
+      gvfs-mount-webdav)  CMD="gvfs-mount -- davs://$STRATO_USERNAME@$STRATO_USERNAME.webdav.hidrive.strato.com:443"
+                          echo "$CMD"
+                          CMD+=" >/dev/null <<<\"$STRATO_PASSWORD\""
+                          eval "$CMD"
+                          ;;
       *) abort "Unsupported mount method \"$MOUNT_METHOD_UNENCRYPTED\" for unencrypted filesystem.";;
     esac
 
-    echo "$CMD"
-    eval "$CMD"
+    # For the 'gvfs-mount method', This is where your system creates the GVFS directory entries with the mount point information:
+    GVFS_MOUNT_DIR="/run/user/$UID/gvfs"
+    # Other possible locations are:
+    #   GVFS_MOUNT_DIR="/run/user/$USER/gvfs"  # For Ubuntu versions 12.10, 13.04 and 13.10.
+    #   GVFS_MOUNT_DIR="$HOME/.gvfs"  # For Ubuntu 12.04 and older.
+
+    case "$MOUNT_METHOD_UNENCRYPTED" in
+      davfs2)  create_or_update_symbolic_link "$MOUNT_LINK" "$MOUNT_POINT_UNENCRYPTED";;
+      gvfs-mount-webdav)
+        GVFS_MOUNT_FILENAME="$GVFS_MOUNT_DIR/dav:host=$STRATO_USERNAME.webdav.hidrive.strato.com,ssl=true,user=$STRATO_USERNAME"
+        if ! [ -d "$GVFS_MOUNT_FILENAME" ]; then
+          abort "The GVFS mountpoint does not exist at \"$GVFS_MOUNT_FILENAME\". Check prerequisites package 'gvfs-fuse' and member of group 'fuse'."
+        fi
+        create_or_update_symbolic_link "$MOUNT_LINK" "$GVFS_MOUNT_FILENAME";;
+      *) abort "Unsupported mount method \"$MOUNT_METHOD_UNENCRYPTED\" for unencrypted filesystem.";;
+    esac
+
+    echo "Unencrypted mountpoint location (via symbolic link):"
+    echo "  $MOUNT_LINK"
 
     if $OPEN_EXPLORER_WINDOW_ON_MOUNT_UNENCRYPTED; then
-      open_explorer_window "$MOUNT_POINT_UNENCRYPTED"
+      open_explorer_window "$MOUNT_LINK"
     fi
 
   fi
@@ -358,7 +545,7 @@ if $SHOULD_MOUNT; then
 
   fi
 
-  LOST_PLUS_FOUND_DIR="$MOUNT_POINT_UNENCRYPTED/lost+found"
+  LOST_PLUS_FOUND_DIR="$MOUNT_LINK/lost+found"
   FILES_IN_LOST_PLUS_FOUND_DIR="$(shopt -s nullglob && shopt -s dotglob && echo $LOST_PLUS_FOUND_DIR/*)"
   if [[ ! -z $FILES_IN_LOST_PLUS_FOUND_DIR ]]; then
     echo "Files detected in the lost+found directory. Opening an explorer window on it."
@@ -381,8 +568,8 @@ else
 
   if $MOUNT_UNENCRYPTED; then
     case "$MOUNT_METHOD_UNENCRYPTED" in
-      webdav)
-        CMD_UNENCRYPTED="sudo umount \"$MOUNT_POINT_UNENCRYPTED\"";;
+      davfs2)  CMD_UNENCRYPTED="sudo umount \"$MOUNT_POINT_UNENCRYPTED\"";;
+      gvfs-mount-webdav) CMD_UNENCRYPTED="gvfs-mount --unmount -- davs://$STRATO_USERNAME@$STRATO_USERNAME.webdav.hidrive.strato.com/";;
       *) abort "Unsupported mount method \"$MOUNT_METHOD_UNENCRYPTED\" for unencrypted filesystem.";;
     esac
 
@@ -406,6 +593,8 @@ else
     echo "Unmounting the Strato Hidrive..."
     echo "$CMD_UNENCRYPTED"
     eval "$CMD_UNENCRYPTED"
+
+    delete_symbolic_link "$MOUNT_LINK"
   fi
 
   echo "Done."
