280aba2b3feeea5fcf7b9129515dd22592a08479
R. Diez <rdiezmail-tools@yahoo.de>
2018-11-29 21:21:22 +0100

    Added TidyUrl.sh

---
+++ TidyUrl/README.txt
@@ -0,0 +1,5 @@
+
+TidyUrl.sh
+
+This script downloads the given URL to a fixed filename under your home directory,
+and runs HTML 'tidy' against it for lint purposes.
---
+++ TidyUrl/TidyUrl.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# This script downloads the given URL to a fixed filename under your home directory,
+# and runs HTML 'tidy' against it for lint purposes.
+#
+# I use it from Emacs to check if an web page generates any HTML warnings.
+#
+# Copyright (c) 2018 R. Diez - Licensed under the GNU AGPLv3
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+declare -r SCRIPT_NAME="TidyUrl.sh"
+
+
+abort ()
+{
+  echo >&2 && echo "Error in script \"$0\": $*" >&2
+  exit 1
+}
+
+
+main ()
+{
+  local URL="$1"
+
+  local DOWNLOAD_DIR="$HOME/.$SCRIPT_NAME-tmp-dir"
+  mkdir --parents -- "$DOWNLOAD_DIR"
+
+  # We could rotate the last N files like script background.sh does.
+  local TMP_FILENAME="$DOWNLOAD_DIR/$SCRIPT_NAME-tmp.html"
+
+  local CMD
+
+  CMD="curl"
+
+  # Option --location makes curl follow redirects.
+  # It does not work with HTML redirects like:  <meta http-equiv="refresh" ...>
+  CMD+=" --location"
+
+  CMD+=" --silent"
+
+  printf -v CMD  "$CMD --output %q  %q"  "$TMP_FILENAME"  "$URL"
+
+  echo "$CMD"
+  eval "$CMD"
+
+  printf -v CMD "tidy --gnu-emacs yes  -quiet -output /dev/null  %q"  "$TMP_FILENAME"
+
+  echo "$CMD"
+  eval "$CMD"
+
+  echo "Finished."
+}
+
+
+if (( $# != 1 )); then
+  abort "You need to pass a single argument with the URL."
+fi
+
+main "$1"
