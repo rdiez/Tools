b5b6cbfde497a4b8c9fbdc84f4e1c60621709cdc
R. Diez <rdiezmail-tools@yahoo.de>
2019-01-26 22:08:24 +0100

    Added Wiki page "Configuring SSH".

--- BackupWikiPages/BackupWikiPages.sh
+++ BackupWikiPages/BackupWikiPages.sh
@@ -89,6 +89,7 @@ place_your_own_urls_here ()
   add_page "Apple_Ger%C3%A4te_haben_in_einem_Hackerspace_nichts_zu_suchen"
   add_page "Rant_about_file_managers"
   add_page "My Chocolatey Notes for Windows"
+  add_page "Configuring SSH"
 
   add_image "3/3a/TightVncServerIcon1.png"
   add_image "9/92/TightVncServerIcon2.png"
