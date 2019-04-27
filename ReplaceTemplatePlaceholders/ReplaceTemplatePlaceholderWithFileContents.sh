a002b6832994746d1e7dcbef0091da1e5be3d8f2
R. Diez <rdiezmail-tools@yahoo.de>
2019-04-27 18:55:57 +0200

    ReplaceTemplatePlaceholderWithFileContents.sh: Just comments.

--- ReplaceTemplatePlaceholders/ReplaceTemplatePlaceholderWithFileContents.sh
+++ ReplaceTemplatePlaceholders/ReplaceTemplatePlaceholderWithFileContents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script version 2.00.
+# Script version 2.01.
 #
 # This script reads a template text file and replaces all occurrences
 # of the given placeholder string with the contents of another file.
@@ -60,6 +60,7 @@ REPLACEMENT_FILENAME="$3"
 
 # I have not found a way yet to make Bash keep the end-of-line characters at the end
 # of the file when using the $(<filename.txt) syntax.
+# Later note: This is probably feasible with mapfile/readarray like this: readarray VAR_NAME < filename.txt
 TEMPLATE_CONTENTS="$(cat "$TEMPLATE_FILENAME" && printf "keep-end-of-lines-at-the-end")"
 TEMPLATE_CONTENTS="${TEMPLATE_CONTENTS%keep-end-of-lines-at-the-end}"
 
@@ -74,5 +75,7 @@ shopt -u extglob
 escape_bash_pattern "$PLACEHOLDER_STRING"
 
 PLACEHOLDER_STRING_ESCAPED="$ESCAPED_BASH_PATTERN"
+
+# Bash does not expand the replacement string. Therefore, REPLACEMENT_STRING does not need to be escaped.
 
 printf "%s" "${TEMPLATE_CONTENTS//$PLACEHOLDER_STRING_ESCAPED/$REPLACEMENT_STRING}"
