e790767eb97848dffa3ce2c242b04cf68d314202
R. Diez <rdiezmail-tools@yahoo.de>
2020-12-07 11:52:57 +0100

    Added some notes to keep-windows-shares-alive.sh .

--- MountWindowsShares/keep-windows-shares-alive.sh
+++ MountWindowsShares/keep-windows-shares-alive.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# keep-windows-shares-alive.sh version 1.00
+# keep-windows-shares-alive.sh version 1.01
 # Copyright (c) 2017 R. Diez - Licensed under the GNU AGPLv3
 #
 # Many Linux kernels have problems with SMB mounts. After a period of inactivity,
 # CIFS connections are severed, and automatic reconnections often do not
 # work properly. You can run this script periodically in order to
 # access your shares at regular intervals and prevent this kind of disconnections.
+#
+# I haven't needed this script for a while now, but I kept it just in case.
 #
 # You will have to manually edit this script in order to specify your mountpoints,
 # see ALL_MOUNTPOINTS below.
@@ -18,6 +20,12 @@
 #
 # Inspect the log file at least once to verify that the script is working properly
 # when started by cron.
+#
+# cron has the habit of e-mailing the user any text that the command outputs. You may only realise
+# when you install an e-mail server on the system, perhaps inadvertently as a prerequisite of some other software.
+# Or you may not notice at all, but the local mail directory for root or your user may just keep forever growing.
+# Therefore, after verifying that the command is working as intended, you may want to append >/dev/null
+# to the cron command, or to drop only all stdout but let stderr through.
 
 set -o errexit
 set -o nounset
