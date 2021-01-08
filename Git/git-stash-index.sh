2c78d073f9c121cd068028c546189ec0eaf0368f
R. Diez <rdiezmail-tools@yahoo.de>
2021-01-08 19:09:21 +0100

    git-unstash-index.sh improved. git-stash-no-index.sh added.

--- Git/git-stash-index.sh
+++ Git/git-stash-index.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Stash only the staged changes, and remove those changes from the stage.
+# Version 1.02.
+#
+# Stash only the staged changes.
+#
+# Those staged changes are then removed from the stage and from the working files too,
+# like "git stash push" normally does.
 #
 # This is useful if you are in the middle of a big commit, and you just realised that
 # you want to make a small, unrelated commit before the big one.
 #
-# After the small commit, restore the previous state with the sibling script unstash-index.sh .
+# Any command-line arguments to this script are passed to the "git stash push" command
+# that ultimately creates the stash from the staged changes.
+# You would normally specify something like --message "my stash message" , or maybe even --include-untracked .
 #
-# Copyright (c) 2017 R. Diez - Licensed under the GNU AGPLv3
+# Copyright (c) 2017-2021 R. Diez - Licensed under the GNU AGPLv3
 
 set -o errexit
 set -o nounset
@@ -16,7 +23,7 @@ set -o pipefail
 # set -x  # Trace execution of this script.
 
 
-declare -r EXIT_CODE_ERROR=1
+declare -r -i EXIT_CODE_ERROR=1
 
 abort ()
 {
@@ -25,12 +32,27 @@ abort ()
 }
 
 
-if [ $# -ne 0 ]; then
-  abort "No command-line arguments are allowed."
-fi
+# Check whether the stage is empty, because using this script makes no sense then,
+# and it is probably a human error.
+
+# --exit-code is actually implied by --quiet, but I like to be explicit.
+CMD="git diff --staged --quiet --exit-code"
+
+echo "$CMD"
+
+set +o errexit
+eval "$CMD"
+GIT_DIFF_EXIT_CODE="$?"
+set -o errexit
+
+case "$GIT_DIFF_EXIT_CODE" in
+  0) abort "The stage/index is empty.";;
+  1) echo "The stage/index is not empty, so carry on.";;
+  *) abort "The \"git diff\" command terminated with unexpected exit code $GIT_DIFF_EXIT_CODE.";;
+esac
 
 
-# POSSIBLE ALTERNATIVE:
+# POSSIBLE ALTERNATIVE IMPLEMENTATION:
 #   We could create a temporary commit with the currently-staged changes,
 #   then create the unrelated commit, afterwards reorder those 2 commits,
 #   and finally restore the originally staged changes.
@@ -39,10 +61,12 @@ fi
 # Step 1)
 #
 # Save all changes in the working tree to the stash, including all staged changes.
+# This stash is only temporary and will be removed afterwards.
+#
 # Revert the working directory to match the HEAD commit, but keep any staged changes.
 # Untracked files are not affected.
 
-CMD="git stash push --quiet --keep-index"
+CMD="git stash push --quiet --keep-index --message \"Temporary stash created by git-stash-index.sh\""
 
 echo "$CMD"
 eval "$CMD"
@@ -50,9 +74,17 @@ eval "$CMD"
 
 # Step 2)
 #
-# Stash everything that remains, which is only the staged changes.
+# Stash everything that remains, which are only the staged changes.
+# This is the stash that we want to keep.
 
-CMD="git stash push --quiet --message \"Created by git-stash-index.sh\""
+CMD="git stash push"
+
+if (( $# != 0 )); then
+
+  printf -v USER_ARGS  " %q"  "$@"
+
+  CMD+="$USER_ARGS"
+fi
 
 echo "$CMD"
 eval "$CMD"
@@ -61,14 +93,44 @@ eval "$CMD"
 # Step 3)
 #
 # Restore what we had at the beginning.
+# The working files include the changes that were staged, but the stage itself is now empty.
 
 CMD="git stash pop --quiet stash@{1}"
 
 echo "$CMD"
 eval "$CMD"
 
-# The stash that remains has just those changes were originally staged.
+
+# Step 4)
+#
+# Remove from the working files all the changes that were staged, because that is what the user
+# probably expects when using a stash command.
+
+# "git apply" must run from the repository's root directory, because according to the documentation:
+#   "When running from a subdirectory in a repository, patched paths outside the directory are ignored."
+# There is no warning or exit code. How user unfriendly is that then?
+
+CMD="git rev-parse --show-toplevel"
+
+echo "$CMD"
+
+ROOT_DIR="$($CMD)"
+
+echo "Changing to the repository's top-level directory before running \"git apply\":"
+echo "  $ROOT_DIR"
+
+pushd "$ROOT_DIR" >/dev/null
+
+CMD="git stash show --patch | git apply --reverse"
+
+echo "$CMD"
+eval "$CMD"
+
+popd >/dev/null
+
+
+# The stash that remains has just the changes that were originally staged.
 
 echo
-echo "Commit your unrelated changes now, and then run git-unstash-index.sh ."
+echo "Commit your unrelated changes now, and then run \"git stash pop --index\" to recover your previous stage/index state."
 echo
---
+++ Git/git-stash-no-index.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Version 1.00.
+#
+# Stash only the changes in the working files that are not staged.
+#
+# Those changes are then removed from the working files, like "git stash push" normally does.
+#
+# This is useful if you have changed a lot of files, and you now want to isolate
+# some of the changes or to temporarily unclutter your workspace. So you stage some changes,
+# but before committing, you want to temporarily remove any other changes,
+# in order to test whether your staged changes compile cleanly.
+#
+# You could branch, but you generally do not want to clutter your history with temporary branches.
+#
+# Any command-line arguments to this script are passed to the "git stash push" command
+# that ultimately creates the stash from the unstaged changes.
+# You would normally specify something like --message "my stash message" , or maybe even --include-untracked .
+#
+# Copyright (c) 2021 R. Diez - Licensed under the GNU AGPLv3
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# set -x  # Trace execution of this script.
+
+
+declare -r -i EXIT_CODE_ERROR=1
+
+abort ()
+{
+  echo >&2 && echo "Error in script \"$0\": $*" >&2
+  exit $EXIT_CODE_ERROR
+}
+
+
+# Check whether the stage is empty, because using this script makes no sense then,
+# and it is probably a human error. There is another reason: this script
+# creates a temporary commit, but such a commit cannot be created if the stage is empty.
+
+# Option --exit-code is actually implied by --quiet, but I like to be explicit.
+CMD="git diff --staged --quiet --exit-code"
+
+echo "$CMD"
+
+set +o errexit
+eval "$CMD"
+GIT_DIFF_EXIT_CODE="$?"
+set -o errexit
+
+case "$GIT_DIFF_EXIT_CODE" in
+  0) abort "The stage/index is empty. This is normally a user error.";;
+  1) echo "The stage/index is not empty, so carry on.";;
+  *) abort "The \"git diff\" command terminated with unexpected exit code $GIT_DIFF_EXIT_CODE.";;
+esac
+
+
+# Step 1)
+#
+# Create a temporary commit to save the staged changes separately.
+#
+# Option --no-verify bypasses the pre-commit and commit-msg hooks. This is not a real,
+# permanent commit, so no other processing should be done with it.
+
+CMD="git commit --quiet --no-verify --message \"Temporary commit created by git-stash-no-index.sh\""
+
+echo "$CMD"
+eval "$CMD"
+
+
+# Step 2)
+#
+# Stash everything that remains, which are only the changes that were not staged.
+
+CMD="git stash push"
+
+if (( $# != 0 )); then
+
+  printf -v USER_ARGS  " %q"  "$@"
+
+  CMD+="$USER_ARGS"
+fi
+
+echo "$CMD"
+eval "$CMD"
+
+
+# Step 3)
+#
+# Drop the temporary commit, so that any changes that were staged at the beginning
+# (before the temporary commit) are considered as "having changed" again.
+#
+# "git reset" does not touch the stage by default, so we add option "--soft" in order
+# for those changes to be staged. This effectively restores the original stage status.
+
+CMD="git reset --soft --quiet HEAD~1"
+
+echo "$CMD"
+eval "$CMD"
+
+
+echo
+echo "Run the usual command \"git stash pop\" later on in order to recover the non-staged changes you just stashed."
+echo
