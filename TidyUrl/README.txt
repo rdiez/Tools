4321c6a2ea4882f542e4f2c45a980aeeb963fc0f
R. Diez <rdiezmail-tools@yahoo.de>
2021-05-07 19:17:31 +0200

    TidyUrl.sh can now run stylelint too for CSS linting.

--- TidyUrl/README.txt
+++ TidyUrl/README.txt
@@ -3,3 +3,5 @@ TidyUrl.sh
 
 This script downloads the given URL to a fixed filename under your home directory,
 and runs HTML 'tidy' against it for lint purposes.
+
+It can optionally run stylelint too for CSS linting.
--- TidyUrl/TidyUrl.sh
+++ TidyUrl/TidyUrl.sh
@@ -5,7 +5,18 @@
 #
 # I use it from Emacs to check if an web page generates any HTML warnings.
 #
-# Copyright (c) 2018 R. Diez - Licensed under the GNU AGPLv3
+# Optional CSS linting:
+#
+#   If environment variable TIDYURL_STYLELINT is set, it is assumed to point to
+#   a directory containing stylelint's configuration file ".stylelintrc.json".
+#
+#   Normally, there is also a subdirectory there called "node_modules", with
+#   yet another subdirectory called "stylelint-config-standard" and so on.
+#
+#   This script will change to the TIDYURL_STYLELINT directory and run stylelint
+#   against the downloaded file too.
+#
+# Copyright (c) 2018-2021 R. Diez - Licensed under the GNU AGPLv3
 
 set -o errexit
 set -o nounset
@@ -19,6 +30,12 @@ abort ()
 {
   echo >&2 && echo "Error in script \"$0\": $*" >&2
   exit 1
+}
+
+
+is_var_set ()
+{
+  if [ "${!1-first}" == "${!1-second}" ]; then return 0; else return 1; fi
 }
 
 
@@ -56,10 +73,36 @@ main ()
   # So I added these markers, in order to quickly locate any HTML warnings in the output.
   echo
   echo "--- Tidy output begin ---"
+
   eval "$CMD"
 
   echo "--- Tidy output end   ---"
   echo
+
+  if is_var_set "TIDYURL_STYLELINT"; then
+
+    pushd "$TIDYURL_STYLELINT" >/dev/null
+
+    # The 'unix' formatter generates warnings like a C compiler does. Emacs' compilation-minor-mode
+    # can automatically hyperlink them to the locations they refer to inside the downloaded HTML file.
+
+    printf -v CMD \
+           "npx stylelint --formatter=unix  %q" \
+           "$TMP_FILENAME"
+
+    echo "$CMD"
+
+    echo
+    echo "--- stylelint output begin ---"
+
+    eval "$CMD"
+
+    echo "--- stylelint output end ---"
+    echo
+
+    popd >/dev/null
+
+  fi
 
   echo "Finished."
 }
