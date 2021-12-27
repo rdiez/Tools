654cc2ee623d5bb80fb6ce555e5c95e2db0396a4
R. Diez <rdiezmail-tools@yahoo.de>
2021-12-27 19:34:28 +0100

    mount-sshfs.sh: Check if already mounted.

--- MountScripts/mount-sshfs.sh
+++ MountScripts/mount-sshfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 1.05.
+# Version 1.06.
 #
 # This is the kind of script I use to conveniently mount and unmount an SSHFS
 # filesystem on a remote host.
@@ -23,7 +23,7 @@
 #   mount-sshfs.sh unmount
 #
 #
-# Copyright (c) 2019 R. Diez - Licensed under the GNU AGPLv3
+# Copyright (c) 2019-2021 R. Diez - Licensed under the GNU AGPLv3
 
 set -o errexit
 set -o nounset
@@ -75,6 +75,106 @@ is_var_set ()
 }
 
 
+# According to the Linux kernel 3.16 documentation, /proc/mounts uses the same format as fstab,
+# which should only escape spaces in the mountpoint (the second field, fs_file).
+# However, another source in the Internet listed the following escaped characters:
+# - space (\040)
+# - tab (\011)
+# - newline (\012)
+# - backslash (\134)
+# That makes sense, so I guess the fstab documentation is wrong.
+# Note that command 'umount' works with the first field (fs_spec) in /etc/mtab, but it takes spaces
+# instead of the escape sequence \040.
+#
+# The kernel documentation does not mention the fact either that the first field (fs_spec)
+# gets escaped too, at least for CIFS (Windows shares) mountpoints.
+#
+# This routine unescapes all octal numeric values with the form "\" + 3 octal digits, not just the ones
+# listed above. It is not clear from the fstab documentation how escaping sequences are generated.
+
+unescape_path()
+{
+  local STILL_TO_PROCESS="$1"
+  local RESULT=""
+
+  # It is not easy to parse strings in bash. There is no "non-greedy" support for regular expressions.
+  # You cannot replace several matches with the result of a function call on the matched text.
+  # Going character-by-character is very slow in a shell script.
+  # Bash can unescape a similar format with printf "%b", but it does not exactly match our escaping specification.
+
+  local REGULAR_EXPRESSION="\\\\([0-7][0-7][0-7])(.*)"
+  local UNESCAPED_CHAR
+  local -i LEN_BEFORE_MATCH
+
+  while [[ $STILL_TO_PROCESS =~ $REGULAR_EXPRESSION ]]; do
+    if false; then
+      echo "Matched: \"${BASH_REMATCH[1]}\", \"${BASH_REMATCH[2]}\""
+    fi
+
+    LEN_BEFORE_MATCH=$(( ${#STILL_TO_PROCESS} - 4 - ${#BASH_REMATCH[2]}))
+    RESULT+="${STILL_TO_PROCESS:0:LEN_BEFORE_MATCH}"
+    printf -v UNESCAPED_CHAR "%b" "\\0${BASH_REMATCH[1]}"
+    RESULT+="$UNESCAPED_CHAR"
+    STILL_TO_PROCESS=${BASH_REMATCH[2]}
+  done
+
+  RESULT+="$STILL_TO_PROCESS"
+
+  UNESCAPED_PATH="$RESULT"
+}
+
+
+declare -A  DETECTED_MOUNT_POINTS  # Associative array.
+
+read_proc_mounts ()
+{
+  # We are reading /proc/mounts because it is maintained by the kernel and has the most accurate information.
+  # An alternative would be reading /etc/mtab, but that is maintained in user space by 'mount' and
+  # may become out of sync.
+
+  # Read the whole /proc/swaps file at once.
+  local PROC_MOUNTS_FILENAME="/proc/mounts"
+  local PROC_MOUNTS_CONTENTS
+  PROC_MOUNTS_CONTENTS="$(<$PROC_MOUNTS_FILENAME)"
+
+  # Split on newline characters.
+  local PROC_MOUNTS_LINES
+  mapfile -t PROC_MOUNTS_LINES <<< "$PROC_MOUNTS_CONTENTS"
+
+  local PROC_MOUNTS_LINE_COUNT="${#PROC_MOUNTS_LINES[@]}"
+
+  local LINE
+  local PARTS
+  local REMOTE_DIR
+  local MOUNT_POINT
+
+  for ((i=0; i<PROC_MOUNTS_LINE_COUNT; i+=1)); do
+    LINE="${PROC_MOUNTS_LINES[$i]}"
+
+    IFS=$' \t' read -r -a PARTS <<< "$LINE"
+
+    REMOTE_DIR_ESCAPED="${PARTS[0]}"
+    MOUNT_POINT_ESCAPED="${PARTS[1]}"
+
+    unescape_path "$REMOTE_DIR_ESCAPED"
+    REMOTE_DIR="$UNESCAPED_PATH"
+
+    unescape_path "$MOUNT_POINT_ESCAPED"
+    MOUNT_POINT="$UNESCAPED_PATH"
+
+    DETECTED_MOUNT_POINTS["$MOUNT_POINT"]="$REMOTE_DIR"
+
+  done
+
+  if false; then
+    echo "Contents of DETECTED_MOUNT_POINTS:"
+    for key in "${!DETECTED_MOUNT_POINTS[@]}"; do
+      printf -- "- %s=%s\\n" "$key" "${DETECTED_MOUNT_POINTS[$key]}"
+    done
+  fi
+}
+
+
 printf -v CMD_UNMOUNT \
        "fusermount -u -z -- %q" \
        "$LOCAL_MOUNT_POINT"
@@ -84,51 +184,70 @@ do_mount ()
 {
   local -r SHOULD_OPEN_AFTER_MOUNTING="$1"
 
-  mkdir --parents -- "$LOCAL_MOUNT_POINT"
+  if test "${DETECTED_MOUNT_POINTS[$LOCAL_MOUNT_POINT]+string_returned_ifexists}"; then
 
-  if ! is_dir_empty "$LOCAL_MOUNT_POINT"; then
-    abort "Mount point \"$LOCAL_MOUNT_POINT\" is not empty (already mounted?). While not strictly a requirement for mounting purposes, this script does not expect a non-empty mountpoint."
+    local MOUNTED_REMOTE_DIR="${DETECTED_MOUNT_POINTS[$LOCAL_MOUNT_POINT]}"
+
+    if [[ $MOUNTED_REMOTE_DIR != "$REMOTE_PATH" ]]; then
+      abort "Mountpoint \"$LOCAL_MOUNT_POINT\" already mounted. However, it does not reference \"$REMOTE_PATH\" as expected, but \"$MOUNTED_REMOTE_DIR\" instead."
+    fi
+
+    printf "Already mounted \"%s\" on \"%s\".\\n" "$REMOTE_PATH" "$LOCAL_MOUNT_POINT"
+
+  else
+
+    mkdir --parents -- "$LOCAL_MOUNT_POINT"
+
+    if ! is_dir_empty "$LOCAL_MOUNT_POINT"; then
+      abort "Mount point \"$LOCAL_MOUNT_POINT\" is not empty (already mounted?). While not strictly a requirement for mounting purposes, this script does not expect a non-empty mountpoint."
+    fi
+
+    verify_tool_is_installed "$SSHFS_TOOL" "sshfs"
+
+    local SSHFS_OPTIONS=""
+
+    # Note that option 'reconnect' will not work with password authentication, or if the SSH key
+    # is password protected and you are not using an SSH agent.
+    SSHFS_OPTIONS+=" -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 "
+
+    # Option 'auto_cache' means "enable caching based on modification times", it can improve performance but maybe risky.
+    # Option 'kernel_cache' could improve performance.
+    # Option 'compression=no' improves performance if you are largely transferring encrypted data, which normally does not compress.
+    # Option 'large_read' could improve performance.
+    SSHFS_OPTIONS+=" -oauto_cache,kernel_cache,compression=no,large_read "
+
+    # This workaround is often needed, for example by rsync.
+    SSHFS_OPTIONS+=" -o workaround=rename "
+
+    # Option 'Ciphers=arcfour' reduces encryption CPU overhead at the cost of security. But this should not matter much because
+    #                          you will probably be using an encrypted filesystem on top.
+    #                          Some SSH servers reject this cipher though, and all you get is an "read: Connection reset by peer" error message.
+
+    # One option I still have to investigate is 'transform_symlinks', which converts absolute links on the remote machine
+    # to relative, since the root directory is different when mounted with SSHFS.
+
+    SSHFS_OPTIONS+=" -o uid=\"$(id --user)\",gid=\"$(id --group)\" "
+
+    local CMD_MOUNT
+    printf -v CMD_MOUNT \
+           "%q %s -- %q  %q" \
+           "$SSHFS_TOOL" \
+           "$SSHFS_OPTIONS" \
+           "$REMOTE_PATH" \
+           "$LOCAL_MOUNT_POINT"
+
+    echo "$CMD_MOUNT"
+    eval "$CMD_MOUNT"
+
+    printf "Mounted \"%s\" on \"%s\".\\n" "$REMOTE_PATH" "$LOCAL_MOUNT_POINT"
+
+    # This hint should not be necessary. After all, this script can unmount too,
+    # and there is only one mountpoint to worry about.
+    if false; then
+      echo "In case something fails, the command to manually unmount is: $CMD_UNMOUNT"
+    fi
+
   fi
-
-  verify_tool_is_installed "$SSHFS_TOOL" "sshfs"
-
-  local SSHFS_OPTIONS=""
-
-  # Note that option 'reconnect' will not work with password authentication, or if the SSH key
-  # is password protected and you are not using an SSH agent.
-  SSHFS_OPTIONS+=" -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 "
-
-  # Option 'auto_cache' means "enable caching based on modification times", it can improve performance but maybe risky.
-  # Option 'kernel_cache' could improve performance.
-  # Option 'compression=no' improves performance if you are largely transferring encrypted data, which normally does not compress.
-  # Option 'large_read' could improve performance.
-  SSHFS_OPTIONS+=" -oauto_cache,kernel_cache,compression=no,large_read "
-
-  # This workaround is often needed, for example by rsync.
-  SSHFS_OPTIONS+=" -o workaround=rename "
-
-  # Option 'Ciphers=arcfour' reduces encryption CPU overhead at the cost of security. But this should not matter much because
-  #                          you will probably be using an encrypted filesystem on top.
-  #                          Some SSH servers reject this cipher though, and all you get is an "read: Connection reset by peer" error message.
-
-  # One option I still have to investigate is 'transform_symlinks', which converts absolute links on the remote machine
-  # to relative, since the root directory is different when mounted with SSHFS.
-
-  SSHFS_OPTIONS+=" -o uid=\"$(id --user)\",gid=\"$(id --group)\" "
-
-  local CMD_MOUNT
-  printf -v CMD_MOUNT \
-         "%q %s -- %q  %q" \
-         "$SSHFS_TOOL" \
-         "$SSHFS_OPTIONS" \
-         "$REMOTE_PATH" \
-         "$LOCAL_MOUNT_POINT"
-
-  echo "$CMD_MOUNT"
-  eval "$CMD_MOUNT"
-
-  echo "In case something fails, the command to manually unmount is: $CMD_UNMOUNT"
-
 
   if $SHOULD_OPEN_AFTER_MOUNTING; then
     local CMD_OPEN_FOLDER
@@ -152,8 +271,20 @@ do_mount ()
 
 do_unmount ()
 {
-  echo "$CMD_UNMOUNT"
-  eval "$CMD_UNMOUNT"
+  if test "${DETECTED_MOUNT_POINTS[$LOCAL_MOUNT_POINT]+string_returned_ifexists}"; then
+
+    local MOUNTED_REMOTE_DIR="${DETECTED_MOUNT_POINTS[$LOCAL_MOUNT_POINT]}"
+
+    if [[ $MOUNTED_REMOTE_DIR != "$REMOTE_PATH" ]]; then
+      abort "Mountpoint \"$LOCAL_MOUNT_POINT\" does not reference \"$REMOTE_PATH\" as expected, but \"$MOUNTED_REMOTE_DIR\" instead."
+    fi
+
+    echo "$CMD_UNMOUNT"
+    eval "$CMD_UNMOUNT"
+
+  else
+    printf "Remote path \"%s\" is not mounted.\\n" "$REMOTE_PATH"
+  fi
 }
 
 
@@ -192,6 +323,8 @@ elif (( $# == 1 )); then
 else
   abort "Invalid arguments. $CMD_LINE_ERR_MSG"
 fi
+
+read_proc_mounts
 
 
 case "$MODE" in
