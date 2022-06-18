42a4b26277164fa6b52714ac090f19ee4e8abca5
R. Diez <rdiezmail-tools@yahoo.de>
2022-06-18 17:04:25 +0200

    clean-git-repo.sh: Reset Git submodules too.

--- Git/clean-git-repo.sh
+++ Git/clean-git-repo.sh
@@ -14,10 +14,23 @@ set -o nounset
 set -o pipefail
 
 # Any changes to tracked files in the working tree and index are discarded.
-git reset --hard origin/master
+git reset --hard origin/master  --recurse-submodule
+
+# The "git reset" may fail in the submodules under some circumstances.
+#
+# Instead of --recurse-submodule, some people do this instead:
+#   git submodule sync --recursive
+#   git submodule update --init --force --recursive
+#
+# This way is allegedly safer, but it takes longer:
+#   # unbinds all submodules
+#   git submodule deinit --force .
+#   # checkout again
+#   git submodule update --init --recursive
 
 # git-clean: Remove untracked files from the working tree.
 #   -d: Remove untracked directories in addition to untracked files.
-#   -x: Don’t use the standard ignore rules read from .gitignore etc.
-#   -f: Force.
-git clean -d -x -f
+#   -x: Don't use the standard ignore rules read from .gitignore etc.
+git clean -d -x --force --force
+
+git submodule foreach --recursive  git clean -d -x --force --force
