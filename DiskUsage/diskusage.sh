66ca3cf2909a06e86534df4cd4105c9ba9766bef
R. Diez <rdiezmail-tools@yahoo.de>
2026-06-22 21:33:40 +0200

    diskusage.sh: The 'sort' option '--key' was not quite right.

--- DiskUsage/diskusage.sh
+++ DiskUsage/diskusage.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Copyright (c) 2022 R. Diez - Licensed under the GNU AGPLv3
+# Copyright (c) 2022-2026 R. Diez - Licensed under the GNU AGPLv3
+#
+# Script version 1.01.
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
 
+declare -r SCRIPT_NAME="${BASH_SOURCE[0]##*/}"  # This script's filename only, without any path components.
+
 declare -r -i EXIT_CODE_ERROR=1
 
 abort ()
 {
-  echo >&2 && echo "Error in script \"$0\": $*" >&2
+  echo >&2 && echo "Error in script \"$SCRIPT_NAME\": $*" >&2
   exit "$EXIT_CODE_ERROR"
 }
 
@@ -24,11 +28,12 @@ fi
 
 printf  -v QUOTED_PARAMS " %q"  "$@"
 
-# 'sort' options:
+# 'sort' options (use '--debug' to troubleshoot):
+# b = ignore leading blanks, otherwise blanks (even separators) are included in the sorting
 # hr = --human-numeric-sort and --reverse
 # f = --ignore-case
 
-CMD="du  --bytes  --human-readable  --summarize  --si  $QUOTED_PARAMS  |  sort  --key=1hr,2f"
+declare -r CMD="du  --bytes  --human-readable  --summarize  --si  $QUOTED_PARAMS  |  sort  --key=1bhr,1  --key=2bf,2"
 
 echo "$CMD"
 echo
